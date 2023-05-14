b8252d0cd76ee341f52ef8dfde838ca38634425d
Yash Sahane <yashsahane23@gmail.com>
2023-05-14 17:34:11 +0530

    some styling

--- multi-step-validation/src/components/Step1.jsx
+++ multi-step-validation/src/components/Step1.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import './step1.css'
 
 const Step1 = ({ nextStepHandler, formData, setFormData }) => {
     const [errors, setErrors] = useState({});
@@ -13,15 +14,15 @@ const Step1 = ({ nextStepHandler, formData, setFormData }) => {
         const { name, email, phone } = formData;
         const stepErrors = {};
         if (!name.trim()) {
-            stepErrors.name = 'Name is required';
+            stepErrors.name = 'This field is required';
         }
         if (!email.trim()) {
-            stepErrors.email = 'Email is required';
+            stepErrors.email = 'This field is required';
         } else if (!/\S+@\S+\.\S+/.test(formData.email)) {
             stepErrors.email = 'Invalid email';
         }
         if (!phone.trim()) {
-            stepErrors.phone = 'Phone Number is required';
+            stepErrors.phone = 'This field is required';
         } else if (!/^(\d{3})[- ]?(\d{3})[- ]?(\d{4})$/.test(phone)) {
             stepErrors.phone = 'Invalid phone number';
         }
@@ -34,19 +35,31 @@ const Step1 = ({ nextStepHandler, formData, setFormData }) => {
     }
 
     return (
-        <div className="step1-div">
-            <h2>Personal info</h2>
-            <p>Please provide your name, email address, and phone number.</p>
-            <form onSubmit={validateStep}>
-                <label htmlFor="name">Name</label>
-                <input type="text" name='name' value={formData.name} placeholder='eg. Stephen King' onChange={changeHandler} />
-                {errors.name && <p>{errors.name}</p>}
-                <label htmlFor="email">Email Address</label>
-                <input type="text" name="email" value={formData.email} placeholder='eg. stephenking@lorem.com' onChange={changeHandler} />
-                {errors.email && <p>{errors.email}</p>}
-                <label htmlFor="phone">Phone Number</label>
-                <input type="number" name="phone" value={formData.phone} placeholder='eg. +1 234 567 890' onChange={changeHandler} />
-                {errors.phone && <p>{errors.phone}</p>}
+        <div>
+            <h1 className='text-4xl font-bold'>Personal info</h1>
+            <p className='text-coolGray mt-2'>Please provide your name, email address, and phone number.</p>
+            <form onSubmit={validateStep} className='mt-8'>
+                <div className='flex flex-col'>
+                    <div className='flex justify-between'>
+                        <label htmlFor="name">Name</label>
+                        {errors.name && <p className='text-strawberryRed font-bold text-sm'>{errors.name}</p>}
+                    </div>
+                    <input type="text" name='name' className='p-2 outline-none border-2 rounded-lg border-coolGray opacity-50 mt-1 font-semibold text-' value={formData.name} placeholder='eg. Stephen King' onChange={changeHandler} />
+                </div>
+                <div>
+                    <div>
+                        <label htmlFor="email">Email Address</label>
+                        {errors.email && <p className='text-strawberryRed'>{errors.email}</p>}
+                    </div>
+                    <input type="text" name="email" value={formData.email} placeholder='eg. stephenking@lorem.com' onChange={changeHandler} />
+                </div>
+                <div>
+                    <div>
+                        <label htmlFor="phone">Phone Number</label>
+                        {errors.phone && <p className='text-strawberryRed'>{errors.phone}</p>}
+                    </div>
+                    <input type="number" name="phone" value={formData.phone} placeholder='eg. +1 234 567 890' onChange={changeHandler} />
+                </div>
                 <button type='submit'>Next Step</button>
             </form>
         </div>
